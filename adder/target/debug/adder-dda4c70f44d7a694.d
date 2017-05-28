708faca1dadf48052e7f85f52aecc58b208a68d1
kota.kaneko <kota.kaneko@livesense.co.jp>
2017-05-28 22:11:30 +0900

    #add test.

---
+++ adder/target/debug/adder-dda4c70f44d7a694.d
@@ -0,0 +1,1 @@
+/Users/Spyc/rust/cargos/adder/target/debug/adder-dda4c70f44d7a694: /Users/Spyc/rust/cargos/adder/src/lib.rs
---
+++ adder/target/debug/libadder.d
@@ -0,0 +1,1 @@
+/Users/Spyc/rust/cargos/adder/target/debug/libadder.rlib: /Users/Spyc/rust/cargos/adder/src/lib.rs
