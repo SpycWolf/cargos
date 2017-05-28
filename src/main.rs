b441091b156e460a0ebb8ded4e8660a9285ae2c2
kota.kaneko <kota.kaneko@livesense.co.jp>
2017-05-28 19:17:53 +0900

    #modify Cargo.toml, and add main logic.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern create rand;
+
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello World!")
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rang().gen_range(1, 101);
+
+    println!("The secret number is: {}, secret_number");
+
+    println!("Please input your geuss.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    println!("Your geussed: {}", guess);
 }
